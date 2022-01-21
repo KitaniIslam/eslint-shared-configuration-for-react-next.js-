196a390ba69b4f43ca9ed9659ed86c0dabfbf7fc
Kitani Islam <slmkitani@gmail.com>
2022-01-21 13:10:48 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const base = require("./lib/base");
+const next = require("./lib/next.js");
+const react = require("./lib/react.js");
+
+module.exports = {
+  extends: ["airbnb", "airbnb/hooks", "plugin:prettier/recommended"],
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ["simple-import-sort", "import"],
+  rules: {
+    ...base,
+    ...next,
+    ...react,
+  },
+  settings: {
+    react: {
+      // Tells eslint-plugin-react to automatically detect the version of React to use
+      version: "detect",
+    },
+    "import/resolver": {
+      alias: {
+        map: [["@", "./src"]],
+        extensions: [".js", ".jsx"],
+      },
+    },
+  },
+};
